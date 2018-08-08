b3dce2ed06859abca52c21c3879d0fd75caab18f
Arthur Hovanesian <nameonascreen2@gmail.com>
2018-08-07 20:41:22 -0700

    Created post and put request handlers

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,4 +1,6 @@
 const models = require('../models');
+const createReview = require('./generator.js');
+const db = require('../models');
 
 module.exports = {
   get: (req, res) => {
@@ -9,5 +11,30 @@ module.exports = {
         res.status(200).send(reviews);
       }
     });
+  },
+  post: (req, res) => {
+    const review = createReview(Number(req.params.roomId));
+    const reviewItem = new db.Review(review);
+    reviewItem.save()
+      .then(() => res.status(201).send())
+      .catch(err => res.status(500).send(err));
+  },
+  put: (req, res) => {
+    const {
+      roomId,
+      reviewer,
+      field,
+      value,
+    } = req.params;
+    models.Review.findOne({ room_id: roomId, user: reviewer })
+      .then((doc) => {
+        const updated = {};
+        updated[field] = value;
+        doc.set(updated);
+        doc.save()
+          .then(result => res.status(201).send(result))
+          .catch(err => res.status(500).send(err));
+      })
+      .catch(err => res.status(500).send(err));
   },
 };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const controller = require('../controllers');
 
 const router = express.Router();
 router.get('/rooms/:roomId/reviews', controller.reviews.get);
+router.post('/rooms/:roomId/newReview', controller.reviews.post);
+router.put('/rooms/:roomId/:reviewer/:field/:value', controller.reviews.put);
 
 module.exports = router;
